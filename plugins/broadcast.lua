5187c5e13004123f4e39f4af285b2c59c429088b
hacker44-h44 <hacker.ht44@gmail.com>
2016-02-24 23:37:01 +0330

    Added files via upload

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -17,9 +17,13 @@ local function run(msg, matches)
 	end
 end
 return {
+	usage ={
+		"broadcast: Send A Message To All Groups.",
+		"bc [id]: Send A Message To Target Group.",
+		},
   patterns = {
-    "^[!/](broadcast) +(.+)$",
-    "^[!/](bc) (%d+) (.*)$"
+    "^(broadcast) +(.+)$",
+    "^(bc) (%d+) (.*)$"
   },
   run = run
 }
